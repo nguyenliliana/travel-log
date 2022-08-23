19aa49ee01e0ed678171f4eca4a8a3c5a6c3adf0
Liliana <liliananguyen03@gmail.com>
2022-08-22 21:25:54 -0700

    completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,27 @@
 //import logo from './logo.svg';
 import './App.css';
+import data from "./data"
 
 import React from "react"
 import Navbar from "./components/Navbar"
 import Entry from "./components/Entry"
 
 export default function App() {
+  const entries = data.map(item => {
+    return (
+      <Entry
+        title={item.title}
+        {...item}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
-      <Entry />
+      <section className="entries">
+        {entries}
+      </section>
     </div>
   )
 }
--- my-app/src/components/Entry.js
+++ my-app/src/components/Entry.js
@@ -1,18 +1,20 @@
 import React from "react"
 import '../App.css'
+
 export default function Entry(props) {
  return (
 
   <div className="entry">
-   <img src='https://source.unsplash.com/WLxQvbMyfas'></img>
+   <img alt="img" className="entry--locationImage" src={props.imageUrl}></img>
    <div className="entry--words">
     <div className="entry--country">
-     <p className="entry--countryText"> JAPAN &nbsp;&nbsp;&nbsp;</p>
-     <a className="entry--countryLink" href="https://goo.gl/maps/1DGM5WrWnATgkSNB8">View on Google Maps</a>
+     <img className="location-symbol" src="../../assets/location-symbol.png" />
+     <p className="entry--countryText"> {props.location} &nbsp;&nbsp;&nbsp;</p>
+     <a className="entry--countryLink" href={props.googleMapsUrl}>View on Google Maps</a>
     </div>
-    <h1 className="entry--location"> Mount Fuji </h1>
-    <p className="entry--date"> 12 Jan, 2021 - 24 Jan, 2021 </p>
-    <p className="entry--description"> Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+    <h1 className="entry--location"> {props.title} </h1>
+    <p className="entry--date"> {props.startDate} - {props.endDate} </p>
+    <p className="entry--description"> {props.description}</p>
    </div>
 
   </div >
